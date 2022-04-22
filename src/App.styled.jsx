b2118127d0b6a071d6677cc01cb46619ca02b853
IrynaMel <iryna.melnyk270692@gmail.com>
2022-04-22 18:03:15 +0300

    add file

--- src/App.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-const Container = styled.div`
-  margin: 25px;
-  font-size: 20px;
-`;
-
-export default Container;
--- src/components/ContactList/ContactList.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const Li = styled.li`
-  margin: 20px;
-  height: 20px;
-  font-size: 20px;
-`;
-
-export default Li;
